251fe71f1242a43833385b5e6e395d01b5e789f4
cris-almodovar <cris.almodovar@gmail.com>
2015-09-09 04:35:29 +0800

    added ext methods

--- TextMatch/Extensions.cs
+++ TextMatch/Extensions.cs
@@ -14,5 +14,15 @@ namespace TextMatch
         {
             (field as Field).SetStringValue(value);
         }
+
+        public static IEnumerable<int> FullTextMatch(this IList<string> items, string queryExpression, int? topN = null)
+        {
+            using (var index = new FullTextIndex())
+            {
+                index.AddItems(items);              
+
+                return index.Search(queryExpression, topN);
+            }
+        }
     }
 }
--- TextMatch/FullTextIndex.cs
+++ TextMatch/FullTextIndex.cs
@@ -6,9 +6,6 @@ using FlexLucene.Search;
 using FlexLucene.Store;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TextMatch
 {
