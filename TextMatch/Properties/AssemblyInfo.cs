e90adc3167049d8a0a34816fe9ae4d0fb547b31e
cris-almodovar <cris.almodovar@gmail.com>
2015-10-13 23:48:06 +0800

    changed assembly description

--- TextMatch/Properties/AssemblyInfo.cs
+++ TextMatch/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TextMatch")]
-[assembly: AssemblyDescription("Full-text matching library")]
+[assembly: AssemblyDescription("A library for matching texts against search expressions")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TextMatch")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
