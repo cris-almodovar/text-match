417fbe2ab728ecee968d72921baf536b68c52a0a
cris-almodovar <cris.almodovar@gmail.com>
2015-10-14 00:38:41 +0800

    created nuget package

--- TextMatch/Properties/AssemblyInfo.cs
+++ TextMatch/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TextMatch")]
-[assembly: AssemblyDescription("A library for matching texts against search expressions")]
+[assembly: AssemblyDescription("A library for matching texts with Lucene query expressions")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TextMatch")]
